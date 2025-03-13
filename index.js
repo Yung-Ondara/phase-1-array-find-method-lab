0df44dc965581682d37673d27bb2228e38663d60
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-13 12:21:12 +0300

    complete array find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(game =>game.result ==="W")
+    return winningYear ? winningYear.year : undefined
+}
+// Example usage:
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+];
+
+console.log(superbowlWin(record)); // Output: "2015
